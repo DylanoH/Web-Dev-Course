0ca361433571d16774d63e5ed062789877342444
Dylano Hartman <dylano.hartman@gmail.com>
2017-07-21 02:41:32 +0200

    Weather App update

--- FreeCodeCamp/Weather App/mainJS.js
+++ FreeCodeCamp/Weather App/mainJS.js
@@ -1,5 +1,8 @@
 // api.openweathermap.org/data/2.5/weather?lat=35&lon=139
 
+$(document).ready(function() {
+
+})
 
 var currentLat;
 var currentLong;
@@ -12,12 +15,53 @@ if (navigator.geolocation) {
 
     //console.log ()
 
-   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + currentLat + "&lon=" + currentLong + "&APPID=" + apiKey, function(data){
-     $("#showWeather").html("latitude: " + currentLat + "<br/>" + "longitude: " + currentLong + "<br/>");
-   });
-//data.weather[0].icon
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + currentLat + "&lon=" + currentLong + "&units=metric" + "&APPID=" + apiKey, function(data){
+      //$("#showWeather").html("latitude: " + currentLat + "<br/>" + "longitude: " + currentLong + "<br/>");
+      // $("#showWeather").html("Temperature: " + data.main.temp + "&deg;" + "<br/>" + "Humidity: " + data.main.humidity);
+      $("#showLocation").html(data.name);
+      $("#showDegrees").html(Math.floor(data.main.temp) + "&deg;");
+      $("#showHumidty").html(data.main.humidity + "%");
 
-    $("#showWeather").html("latitude: " + currentLat + "<br/>" + "longitude: " + currentLong);
+      weatherTypes();
+
+      function weatherTypes() {
+        switch (data.weather[0].description) {
+          case "clear sky":
+            $(".weatherType").html("clear sky");
+            //$("body").css("background-image", "url('Resources/clearsky.jpg')")
+            break;
+          case "few clouds":
+            $(".weatherType").html("few clouds");
+            break;
+          case "scattered clouds":
+            $(".weatherType").html("scattered clouds");
+            break;
+          case "broken clouds":
+            $(".weatherType").html("broken clouds");
+            break;
+          case "shower rain":
+            $(".weatherType").html("shower rain");
+            break;
+          case "rain":
+            $(".weatherType").html("rain");
+            break;
+          case "thunderstorm":
+            $(".weatherType").html("thunderstorm");
+            break;
+          case "snow":
+            $(".weatherType").html("snow");
+            break;
+          case "mist":
+            $(".weatherType").html("mist");
+            break;
+        }
+      }
+
+
+    }); // json end
+
+
+    // $("#showWeather").html("latitude: " + currentLat + "<br/>" + "longitude: " + currentLong);
     console.log("lat: "+currentLat + " long: " + currentLong);
-  });
+  }); // navigator end
 }
