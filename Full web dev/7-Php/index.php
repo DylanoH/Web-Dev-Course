3c7e56b496dabf3c6dfbc2d17bc693de179c47b3
Dylano Hartman <dylano.hartman@gmail.com>
2017-09-18 21:40:51 +0200

    PHP - variables

---
+++ Full web dev/7-Php/index.php
@@ -0,0 +1,22 @@
+<?php
+  $name = "Dylano";
+  echo "<p>My name is $name.</p>";
+
+  $string1 = "<p>this is the first part";
+  $string2 = "of the sentence.</p>";
+
+  echo $string1." ".$string2;
+
+  $myNumber = 45;
+
+  $calculation = $myNumber * 31 / 89 + 4;
+
+  echo "The result is: ".$calculation;
+
+  $myBool = true;
+  echo "<p>Is this true?".$myBool."</p>";
+
+  $variableName = "name";
+
+  echo $$variableName;
+?>
