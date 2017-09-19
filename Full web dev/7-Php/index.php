66ba9e7263de9e1ee822d729f7c52701fdfdeca3
Dylano Hartman <dylano.hartman@gmail.com>
2017-09-19 23:03:40 +0200

    PHP - GET variable prime number check

--- Full web dev/7-Php/index.php
+++ Full web dev/7-Php/index.php
@@ -1,13 +1,32 @@
 <?php
 
-$i = 0;
-$myDearFriends = array("Sammie", "Oona", "Zempy", "Lucy");
+  if($_GET){
 
-while ($i < sizeOf($myDearFriends)) {
+    $isPrime = true;
 
-  echo $myDearFriends[$i]."<br />";
+    for($i = 2; $i < $_GET["number"]; $i++) {
+      if($_GET["number"] % $i == 0) {
+        $isPrime = false;
+      }
+    }
 
-  $i++;
-}
+    if ($isPrime == true) {
+      echo "<p>".$i." is a prime number!</p>";
+    }
+    else {
+      echo "<p>".$i." is not prime!</p>";
+    }
+  }
+
+  // echo "Hi there ".$_GET["number"]."!";
+
+
 
 ?>
+
+<p>Enter a whole number</p>
+
+<form class="" >
+  <input type="number" name="number" >
+  <input type="submit" value="go!" >
+</form>
