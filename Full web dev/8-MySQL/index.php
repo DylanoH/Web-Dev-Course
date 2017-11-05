016ea68f2c22db66ed0981c7748b3fb2138daab8
Dylano Hartman <dylano.hartman@gmail.com>
2017-11-05 23:39:48 +0100

    MySQL - INSERT INTO and UPDATE queries

--- Full web dev/8-MySQL/index.php
+++ Full web dev/8-MySQL/index.php
@@ -6,8 +6,16 @@
     die ("There was an error connecting to the database");
   }
 
-  $query = "SELECT * FROM users";
+  // $query = "INSERT INTO `users` (`email`, `password`) VALUES('priscillahartman@gmail.com', 'paard7')";
+  // mysqli_query($link, $query);
 
+  // $query = "UPDATE `users` SET email = 'dylanohartman@gmail.com' WHERE id = 1 LIMIT 1";
+  // mysqli_query($link, $query);
+
+  $query = "UPDATE `users` SET password = '8kUeeRdhe9Wc2Zp3' WHERE email = 'dylanohartman@gmail.com' LIMIT 1";
+  mysqli_query($link, $query);
+
+  $query = "SELECT * FROM users";
   if($result = mysqli_query($link, $query)) {
     $row = mysqli_fetch_array($result);
     echo "your email is: ".$row[email]." and your password is ".$row[password];
