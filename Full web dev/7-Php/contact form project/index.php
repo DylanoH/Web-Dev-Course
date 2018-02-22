1d748de00dc6afa50d5968311627104c23c9703b
Dylano Hartman <dylano.hartman@gmail.com>
2018-02-22 23:44:36 +0100

    PHP finished contact form

--- Full web dev/7-Php/contact form project/index.php
+++ Full web dev/7-Php/contact form project/index.php
@@ -1,3 +1,58 @@
+<?php
+
+$error = "";
+$successMessage = "";
+
+  if ($_POST) {
+
+      if (!$_POST["email"]) {
+
+          $error .= "An email address is required.<br>";
+
+      }
+
+      if (!$_POST["content"]) {
+
+          $error .= "The content field is required.<br>";
+
+      }
+
+      if (!$_POST["subject"]) {
+
+          $error .= "The subject is required.<br>";
+
+      }
+
+      if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+
+          $error .= "The email address is invalid.<br>";
+
+      }
+
+      if ($error != "") {
+
+        $error = '<div class="alert alert-danger" role="alert"><p>There were error(s) in your form:</p>' . $error . '</div>';
+      }
+      else {
+
+        $emailTo = "dylano.hartman@gmail.com";
+        $subject = $_POST['subject'];
+        $content = $_POST['content'];
+        $headers = "From: ".$_POST['email'];
+
+        if (mail($emailTo, $subject, $content, $headers)) {
+          $successMessage = '<div class="alert alert-success" role="alert">Your message was sent, we\'ll get back to you ASAP!</div>';
+        }
+        else {
+          $error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later</div>';
+        }
+
+
+      }
+  }
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -16,7 +71,9 @@
 
     <div class="container">
       <h1>Get in touch!</h1>
-        <div id="error"></div>
+
+        <div id="error"><?php echo $error; ?></div>
+
         <form method="post">
           <fieldset class="form-group">
             <label for="email">Email address</label>
@@ -38,6 +95,6 @@
     <script src="jquery/jquery.easing.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="javascripts/script.js"></script>
+    <script type="text/javascript" src="javascript/script.js"></script>
   </body>
 </html>
