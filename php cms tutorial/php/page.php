2aa21b5d90ab44cb0a0d975eb3ee7742342d6879
Dylano Hartman <dylano.hartman@gmail.com>
2018-03-15 23:58:49 +0100

    php cms tutorial 4,5 part video

--- php cms tutorial/php/page.php
+++ php cms tutorial/php/page.php
@@ -1,1 +1,25 @@
-page
+<?php
+  require 'app/start.php';
+
+  if (empty($_GET['page'])) {
+    $page = false;
+  }
+  else {
+    $slug = $_GET['page'];
+
+    $page = $con->prepare("
+      SELECT *
+      FROM pages
+      WHERE slug = ?
+    ");
+
+    $page->bind_param("s", $slug);
+
+    $page->execute();
+
+    $result = $page->get_result();
+
+    var_dump($result);
+  }
+
+?>
